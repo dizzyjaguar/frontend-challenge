b98cac99992a942575d54c1d4aef49e17c8d89ba
Eli Zevin <dizzyjaguar@gmail.com>
2023-03-25 14:59:34 -0700

    Create Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,28 @@
+interface ButtonProps {
+  text: string
+  loading?: Boolean
+  handleClick?: () => {}
+}
+
+/**
+ * Button
+ * 
+ * @prop text - text content of the `button`
+ * @prop loading - `boolean` loading state
+ * @prop handleClick - `function` that runs `onClick`
+ */
+
+function Button({ text, loading, handleClick }: ButtonProps) {
+  return (
+    <button
+      onClick={!loading ? handleClick : undefined}
+      className={`w-full h-10 bg-[#C4232A] text-white hover:opacity-90 hover:text-gray-50 active:bg-red-700 ${
+        loading && 'bg-gray-400 active:bg-gray-400'
+      }`}
+    >
+      {!loading ? text : 'Loading...'}
+    </button>
+  )
+}
+
+export default Button
