9c9e74425a0cd0e79ec0d7e8c5f3d8ba37ee7b96
Eli Zevin <dizzyjaguar@gmail.com>
2023-03-26 11:38:56 -0700

    Create useFeed hook

--- src/hooks/feed.ts
+++ src/hooks/feed.ts
@@ -1,1 +1,24 @@
-export {}
+import axios from 'axios'
+import useSWRInfinite from 'swr/infinite'
+import { API_URL } from '../constants'
+
+const fetcher = (url: string) => axios.get(url).then(res => res.data)
+
+const useFeed = () => {
+  // use SWR to be our workhorse
+  const { data, error, isLoading, size, setSize } = useSWRInfinite(
+    index => API_URL + `?page=${index + 1}`,
+    fetcher
+  )
+  // add the next group of stories to the end of the previously fetched stories
+  const stories = data ? [].concat(...data) : []
+  const isLoadingMore =
+    isLoading || (size > 0 && data && typeof data[size - 1] === 'undefined')
+
+  // fetch the next page of stories
+  const fetchMore = () => setSize(size + 1)
+
+  return { stories, error, isLoading, isLoadingMore, fetchMore }
+}
+
+export default useFeed
