88eaaa0b4c6a548a4cdf5dc3a2e48ec535e732d3
Eli Zevin <dizzyjaguar@gmail.com>
2023-04-11 19:46:08 -0700

    Update Player to recieve props

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,20 +1,21 @@
 // @ts-ignore
 import { AudioPlayer } from '@barstoolsports/react-audio-player'
-import usePodcasts from '../../hooks/podcasts'
+import { useRef } from 'react'
 
-function Player() {
-  const { currentEpisode } = usePodcasts()
-  console.log(currentEpisode)
+const initialState = { title: 'Loading...', link: '' }
+
+function Player({ track = initialState }) {
+  const player = useRef()
+
   return (
     <div className="sticky bottom-0 my-4">
-      {currentEpisode !== null ? (
+      {
         <AudioPlayer
-          trackTitle={currentEpisode ? currentEpisode.title : 'Loading...'}
-          trackUrl={currentEpisode && currentEpisode.link}
+          ref={player}
+          trackTitle={track !== undefined ? track.title : undefined}
+          trackUrl={track !== undefined ? track.link : undefined}
         />
-      ) : (
-        <h1>Loading Audio Player</h1>
-      )}
+      }
     </div>
   )
 }
